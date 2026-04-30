9a5856e004cd6331b4693d76ec47a6c1f54e03ab
TM_Lechik <aleshaboyko30@yandex.ru>
2026-04-30 22:31:04 +0300

    feat: add admin schemas for issue #43

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,29 +1,9 @@
-from pydantic import BaseModel,ConfigDict
-from datetime import date
-from backend.app.core.role import UserRole
-
-class UserCreate(BaseModel):
-    login: str
-    password: str
-    role: UserRole
-    name: str
-    surname: str
-    organization_id: int
-    patronymic: str
-    date_of_birth: date
-
-class UserOut(BaseModel):
-    id: int
-    login: str
-    role: UserRole
-    name: str
-    surname: str
-    organization_id: int
-    patronymic: str
-    date_of_birth: date
-
-    model_config = ConfigDict(from_attributes=True)
+from pydantic import BaseModel
 
 class UserLogin(BaseModel):
     login: str
     password: str
+
+class TokenOut(BaseModel):
+    access_token: str
+    token_type: str = "bearer"

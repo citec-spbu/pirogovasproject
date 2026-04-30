33d9c0300b13c92f690388174dc330842eaf5453
TM_Lechik <aleshaboyko30@yandex.ru>
2026-04-30 18:13:11 +0300

    feat: bootstrap for admin and database for isssue #42

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,20 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from app.api.v1 import users
 from app.api.v1 import reports
 from app.api.v1 import llm
 
-app = FastAPI(title="CT AI Analysis API")
+from app.core.database import engine
+from app.services.bootstrap_service import bootstrap
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await bootstrap()
+    yield
+    await engine.dispose()
+
+app = FastAPI(title="CT AI Analysis API", lifespan=lifespan)
 
 app.include_router(users.router, prefix="/api/v1")
 app.include_router(reports.router, prefix="/api/v1")
---
+++ backend/app/services/bootstrap_service.py
@@ -0,0 +1,55 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.config import get_settings
+from app.models.user import User, Organization
+from app.core.role import UserRole
+from app.core.security import get_password_hash
+from app.core.database import engine, Base, AsyncSessionLocal
+
+async def create_tables() -> None:
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+async def bootstrap_first_admin(db: AsyncSession) -> None:
+    admin_exists_result = await db.execute(
+        select(User).where(User.role == UserRole.ADMIN)
+    )
+    admin_exists = admin_exists_result.scalar_one_or_none()
+
+    if admin_exists:
+        return
+    
+    organization_exists_result = await db.execute(
+        select(Organization).where(Organization.name == get_settings().FIRST_ORGANIZATION_NAME)
+    )
+    organization = organization_exists_result.scalar_one_or_none()
+
+    if not organization:
+        organization = Organization(
+            name=get_settings().FIRST_ORGANIZATION_NAME
+        )
+        db.add(organization)
+        await db.commit()
+
+    first_admin = User(
+        login=get_settings().FIRST_ADMIN_LOGIN,
+        hashed_password=get_password_hash(get_settings().FIRST_ADMIN_PASSWORD),
+        role=UserRole.ADMIN,
+        organization_id=organization.id,
+        name = get_settings().FIRST_ADMIN_NAME
+        surname = get_settings().FIRST_ADMIN_SURNAME
+        patronymic = get_settings().FIRST_ADMIN_PATRONYMIC
+        date_of_birth = get_settings().FIRST_ADMIN_DATE_OF_BIRTH
+        is_active = True
+    )
+
+    db.add(first_admin)
+    await db.commit()
+
+async def bootstrap() -> None:
+    await create_tables()
+
+    async with AsyncSessionLocal() as db:
+        await bootstrap_first_admin(db)
+

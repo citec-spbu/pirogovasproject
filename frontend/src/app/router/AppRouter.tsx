bea1ceee34ffe7fd38c06c8b7f6d2fe9b3e9ed34
jubelyak <120456228+jubelyak@users.noreply.github.com>
2026-05-16 12:20:34 +0300

    fix: approuter

--- frontend/src/app/router/AppRouter.tsx
+++ frontend/src/app/router/AppRouter.tsx
@@ -22,7 +22,7 @@ export const AppRouter = () => {
         path="/admin"
         element={
           <ProtectedRoute allowedRoles={['admin']}>
-           <MainPage />
+           < MainPage/>
           </ProtectedRoute>
         }
       />
